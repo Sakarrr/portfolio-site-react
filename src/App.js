0a95d25ce782b483e1975555a488db7e2fa77076
Sakarr <ctha.sakar@gmail.com>
2024-04-29 12:54:43 +0545

    Add - NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio Site</h1>
-    </div>
+      <body>
+      < NavBar />
+      </body>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import github from '../assets/GitHub.png';
+import linkedin from '../assets/LinkedIn.png';
+import resume from '../assets/Resume.png';
+
+function NavBar() {
+  return (
+      <>
+          <div className="navbar">
+              <div className="container">
+                  <div className="nav-branding">Sakar Shrestha.</div>
+                  <div className="nav-links">
+                      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                      <a href="https://github.com/Sakarrr" target="_blank" rel="noreferrer"><img src={github} alt="github"/></a>
+
+                      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                      <a href="https://www.linkedin.com/in/sakarshrestha97/" target="_blank" rel="noreferrer"><img src={linkedin} alt="github"/></a>
+
+                      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                      <a href="#" target="_blank" rel="noreferrer"><img src={resume} alt="github"/></a>
+                  </div>
+              </div>
+          </div>
+          <div className="line"></div>
+      </>
+  );
+}
+
+export default NavBar;
