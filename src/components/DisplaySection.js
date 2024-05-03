ff129dc529fae94bc5326ada4705e55f9670715d
Sakarr <ctha.sakar@gmail.com>
2024-05-03 21:15:10 +0545

    Update - Language descriptions

--- src/components/DisplaySection.js
+++ src/components/DisplaySection.js
@@ -6,48 +6,55 @@ import javascript from "../assets/JavaScript.png";
 import php from "../assets/PHP.png";
 
 function displaySection() {
-    return (
-        <div className="display-section">
-            <div className="container">
-                <div className="display-content-top">
-                    <div className={"logo-container"}>
-                        <img src={magazinex} alt="MagazineX"/>
-                    </div>
-                    <div className={"logo-container"}>
-                        <img src={magazineblocks} alt="MagazineX"/>
-                    </div>
-                </div>
-                <div className="display-content-bottom">
-                    <h2>Experienced working with <br></br>
-                        <b>Multiple</b> Programming Languages
-                    </h2>
-                    <div className="display-content-bottom-languages">
-                        <span>
-                            <img src={reactjs} alt="ReactJS"/>
-                            <div className={"description"}>
-                                <h4>React.js</h4>
-                                <p>JavaScript library by Facebook.</p>
-                            </div>
-                        </span>
-                        <span>
-                            <img src={javascript} alt="ReactJS"/>
-                            <div className={"description"}>
-                                <h4>JavaScript</h4>
-                                <p>Versatile scripting language used for web development, often associated with client-side interactivity.</p>
-                            </div>
-                        </span>
-                        <span>
-                            <img src={php} alt="ReactJS"/>
-                            <div className={"description"}>
-                                <h4>Php</h4>
-                                <p>Server-side scripting language commonly used for web development, known for its flexibility and integration with databases.</p>
-                            </div>
-                        </span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="display-section">
+      <div className="container">
+        <div className="display-content-top">
+          <div className={"logo-container"}>
+            <img src={magazinex} alt="MagazineX" />
+          </div>
+          <div className={"logo-container"}>
+            <img src={magazineblocks} alt="MagazineX" />
+          </div>
         </div>
-    );
+        <div className="display-content-bottom">
+          <h2>
+            Experienced working with <br></br>
+            <b>Multiple</b> Programming Languages
+          </h2>
+          <div className="display-content-bottom-languages">
+            <span>
+              <img src={reactjs} alt="ReactJS" />
+              <div className={"description"}>
+                <h4>React.js</h4>
+                <p>JavaScript library by Facebook.</p>
+              </div>
+            </span>
+            <span>
+              <img src={javascript} alt="ReactJS" />
+              <div className={"description"}>
+                <h4>JavaScript</h4>
+                <p>
+                  High-Level, Object-Oriented, Multi-Paradigm programming
+                  language.
+                </p>
+              </div>
+            </span>
+            <span>
+              <img src={php} alt="ReactJS" />
+              <div className={"description"}>
+                <h4>Php</h4>
+                <p>
+                  Server-side scripting language, known for its flexibility and
+                  integration with databases.
+                </p>
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default displaySection;
